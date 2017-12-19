8930d615f5ccec6fd78cfb66d2ff4eb3d9803ffd
jhnoor <jamawadi@gmail.com>
2017-12-19 06:01:35 +0100

    added camera proof of concept

--- src/frontend/src/app/app.component.ts
+++ src/frontend/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 
 @Component({
   selector: 'maas-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  @ViewChild('hardwareVideo') hardwareVideo: any;
+
+  constructor() { }
+
+  ngOnInit() {
+    const video = this.hardwareVideo.nativeElement;
+
+    const n = <any>navigator;
+
+    n.getUserMedia = (n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia || n.msGetUserMedia);
+
+    n.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+      video.src = window.URL.createObjectURL(stream);
+      video.play();
+    });
+  }
 }
